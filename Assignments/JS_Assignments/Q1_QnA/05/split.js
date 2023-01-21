96bb5872525752badcdbf0ed6559964c064c5d93
khushi <khushichauhan425@gmail.com>
2023-01-21 19:30:20 +0530

    ques 5 solution added

---
+++ Assignments/JS_Assignments/Q1_QnA/05/split.js
@@ -0,0 +1,6 @@
+// 05. Declare a varibale and assign 
+// string value to it and then split it into an array using split() method.
+
+
+let sentence = "I like to do web development"
+console.log(sentence.split(" "));
