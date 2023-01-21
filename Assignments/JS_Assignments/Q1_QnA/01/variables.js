db9c27576baea2221a682ced4631ca6caba0d00e
khushi <khushichauhan425@gmail.com>
2023-01-21 14:24:40 +0530

    JS Assignment folder added

---
+++ Assignments/JS_Assignments/Q1_QnA/01/variables.js
@@ -0,0 +1,14 @@
+// 01. Create a variable.js file and declare variables and assign string, boolean, undefined 
+// and null data types, Display all the value with their data type.
+
+let name = 'Khushi';
+console.log("Datatype of", name, "is", typeof name);
+
+let age = true;
+console.log("Datatype of", age, "is", typeof age);
+
+let school;
+console.log("Datatype of school  is", typeof school);
+
+let subject = null;
+console.log("Datatype of subject is", typeof subject);
