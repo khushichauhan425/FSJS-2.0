258d057895a68d67d8f62f4887610902911aed72
khushi <khushichauhan425@gmail.com>
2023-01-21 21:22:47 +0530

    ques 9 solution added

---
+++ Assignments/JS_Assignments/Q1_QnA/09/boolean.js
@@ -0,0 +1,32 @@
+//09. Boolean value is either true or false.
+//    - Write three JavaScript statement example which provide truthy value.
+//    - Write three JavaScript statement example which provide falsy value.
+
+// truthy values:-
+let username = "Khushi";
+let age = 23
+let student = true;
+
+if(username){
+    console.log(username);
+}
+if(age){
+    console.log(age);
+}
+if(student){
+    console.log(student);
+}
+
+//falsy values
+let userName;
+let userAge = 0;
+let student1 = false;
+if(userName){
+    console.log(username);
+}
+if(userAge){
+    console.log(age);
+}
+if(student1){
+    console.log(student1);
+}
