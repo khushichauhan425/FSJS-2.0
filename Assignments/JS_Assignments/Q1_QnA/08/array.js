99a766481c43cd8df32733aae5b9aa4094170ae4
khushi <khushichauhan425@gmail.com>
2023-01-21 21:17:11 +0530

    ques 8 solution added

---
+++ Assignments/JS_Assignments/Q1_QnA/08/array.js
@@ -0,0 +1,7 @@
+//08. Demonstrate the use of trim() to remove any trailing 
+// whitespace at the beginning and the end of a string.
+
+
+let sentence = "  hi! how are you doing nowadays!   "
+console.log(sentence);
+console.log(sentence.trim());
