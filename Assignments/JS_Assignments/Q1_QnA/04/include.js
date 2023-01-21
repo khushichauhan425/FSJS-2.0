4ba0d36b357e75eaa904d6f239e9653c23232f9d
khushi <khushichauhan425@gmail.com>
2023-01-21 19:19:39 +0530

    ques 4 solution added

---
+++ Assignments/JS_Assignments/Q1_QnA/04/include.js
@@ -0,0 +1,8 @@
+// 04. Declare a varibale and assign string value to it 
+// and check if the string contains a word Script using includes() method.
+
+//includes() method is case sensitive method.
+
+let sentence = "Welcome to full Stack JS Bootcamp";
+console.log(sentence.includes("Full"));
+console.log(sentence.includes("full"));
