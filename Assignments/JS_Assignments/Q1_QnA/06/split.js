697f14bcc7488bdc9d01deca7dfa1005c9b4e310
khushi <khushichauhan425@gmail.com>
2023-01-21 19:34:31 +0530

    ques 6 solution added

---
+++ Assignments/JS_Assignments/Q1_QnA/06/split.js
@@ -0,0 +1,5 @@
+// 06. 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' 
+// split the string at the comma and change it to an array.
+
+let sentence = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+console.log(sentence.split(","));
