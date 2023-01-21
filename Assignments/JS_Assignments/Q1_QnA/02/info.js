7e0bf754ea4046b8900a3d5dd3bf56c31859e97a
khushi <khushichauhan425@gmail.com>
2023-01-21 19:09:18 +0530

    Ques 2 solution added

---
+++ Assignments/JS_Assignments/Q1_QnA/02/info.js
@@ -0,0 +1,10 @@
+// 02. Declare variables to store your first name, last name, 
+// marital status, country and age and display them using interploation method.
+
+const fname = "Khushi";
+const lname = "Chauhan";
+let marital_status = "unmarried";
+let country = "India";
+let age = 23;
+
+console.log(`My name is ${fname} ${lname} and my age is ${age}. My marital status is ${marital_status}. I live in ${country}.`);
