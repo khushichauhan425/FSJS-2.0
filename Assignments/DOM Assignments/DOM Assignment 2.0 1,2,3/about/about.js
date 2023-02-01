ffd7bce94c51fea8d33b2ab0226fc15c6af290c1
khushi <khushichauhan425@gmail.com>
2023-02-01 23:12:42 +0530

    Second Assignment solution added

--- Assignments/DOM Assignments/DOM Assignment 2.0 1,2,3/about/about.js
+++ Assignments/DOM Assignments/DOM Assignment 2.0 1,2,3/about/about.js
@@ -9,3 +9,50 @@ accordian.forEach((element) => {
     }
   });
 });
+
+//Second Assignment:-
+//Task 1:-
+//1. Change Contact to Projects in header.
+//2. Change the color of the fields to purple.
+
+//Sol 1. Change Contact to Projects in header.
+let atag=document.querySelectorAll("a");
+let contact=Array.from(atag).filter((atag) =>
+    atag.textContent === "Contact")[0];
+contact.innerText = "Projects";
+
+//Sol 2. Change the color of the fields to purple.
+Array.from(accordian);
+accordian.forEach((element) =>{
+  element.style.background = "#DADAF8";
+})
+
+//Task 2:-
+//1. Adding a skills tab.
+//2. Adding content ot it.
+
+//Sol 1. Adding a skills tab.
+let main=document.querySelector(".accordian-wrapper");
+
+let d=document.createElement("div");
+let skill=document.createElement("h3");
+let para=document.createElement("p");
+
+d.className="accordian";
+skill.style.background="rgb(218, 218, 248)";
+skill.innerText="Skills";
+d.appendChild(skill);
+main.append(d);
+
+// //Sol 2. Adding content ot it.
+// d.appendChild(para);
+// // para.onclick=function(){
+// //   add()
+// // };
+// // function add(){
+// //   para.innerText = "I posses a very good command over the Full Stack Development technologies like MERN which can be seen in my work over the Github";
+// // }
+
+// para.addEventListener('click',(event) => {
+//   para.innerText="I posses a very good command over the Full Stack Development technologies like MERN which can be seen in my work over the Github";
+// });
