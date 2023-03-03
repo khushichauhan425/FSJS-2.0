a4b935f63caad093e6692e3da5b3c4b88c45a96d
khushi <khushichauhan425@gmail.com>
2023-03-03 15:32:14 +0530

    project 7 done

--- Assignments/DOM Assignments/DOM P7/DOM P7/index.js
+++ Assignments/DOM Assignments/DOM P7/DOM P7/index.js
@@ -5,3 +5,28 @@ firstarray.forEach((a) => {
         a.style.display='none';
     }
 })
+
+
+let input = document.querySelector(".main__form-input");
+let btn = document.querySelector(".main__form-btn");
+
+input.removeAttribute("disabled");
+btn.removeAttribute("disabled");
+
+btn.addEventListener("click",(e) => {
+    e.preventDefault();
+    localStorage.setItem("inputValue", input.value);
+    location.reload();
+});
+
+
+window.addEventListener("load",() => {
+    const stored = localStorage.getItem("inputValue");
+    input.value = stored;
+    const lang = document.querySelector(".main__languages");
+    const a = document.createElement("a");
+    a.target = 'blank';
+    a.href = 'https://www.ineuron.ai';
+    a.innerText = stored;
+    lang.append(a);
+});
